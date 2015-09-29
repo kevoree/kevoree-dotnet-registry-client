afeb32b4c0ebe187d2bae5676e54b09020b2b6de
Manuel Leduc <manuel.leduc@inria.fr>
2015-09-29 09:30:21 +0200

    Simple registry client
    For now it only use case is publishing a container root in a registry.

---
+++ RegistryClient.cs
@@ -0,0 +1,40 @@
+﻿using org.kevoree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Org.Kevoree.Registry.Client
+{
+    public class RegistryClient
+    {
+        private string baseUrl;
+
+        public RegistryClient(string baseUrl)
+        {
+            this.baseUrl = baseUrl;
+        }
+
+        public async Task publishContainerRoot(ContainerRoot containerRoot)
+        {
+            var saver = new org.kevoree.pmodeling.api.json.JSONModelSerializer();
+            string json = saver.serialize(containerRoot);
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var httpcontent = new StringContent(json, Encoding.UTF8, "application/json");
+                var res = await client.PostAsync("/deploy", httpcontent);
+                if (!res.IsSuccessStatusCode)
+                {
+                    var e = new HttpRequestException();
+                    throw e;
+                }
+            }
+        }
+    }
+}
